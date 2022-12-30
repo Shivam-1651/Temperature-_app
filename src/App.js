3b6440786cc3d6e8765bdf9f005d433a76f72e83
Shivam-1651 <115480237+Shivam-1651@users.noreply.github.com>
2022-12-30 15:44:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tempapp from "./components/Tempapp";
+import './App.css';
+// import "./css/style.cs"
+
+
+function App() {
+  return (
+    <>
+<Tempapp/>
+    </>
+  );
+}
+
+export default App;
